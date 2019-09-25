ba9247c4a7151ceaa82a0a7fbe1348f4c122a781
baharb68 <55702693+baharb68@users.noreply.github.com>
2019-09-25 20:49:05 +0330

    optimizing payment.py

--- payment.py
+++ payment.py
@@ -1,23 +1,17 @@
 DailyPayment = 505000
 salary=0
 kabiseyear=1398
-currentyear=1402
-currentmonth=4
+currentyear=1401
+currentmonth=12
 off=[1,2,3,0,2,4,2,1,2,3,0,2]
 count=0
-
+day=30
 if currentmonth<=6:
     day=31
-elif currentmonth<=11:
-    day=30
-else:
-    if (currentyear-kabiseyear)%4 == 0:
-        day=30
-    else:
-        day=29
+elif(currentmonth==12 and (currentyear-kabiseyear)%4 != 0):
+    day=29
 for i in range(day):
     if (i+1)%7 !=0:
         count=count+1
 salary=(count*DailyPayment)-(off[currentmonth-1]*DailyPayment)
 print(currentmonth,":",salary)
-
